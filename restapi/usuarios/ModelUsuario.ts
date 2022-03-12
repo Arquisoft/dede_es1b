cb9877ff7b71a904b5e1fe20d4596c879d2e10e8
UO277646 <UO277646@uniovi.es>
2022-03-12 11:27:19 +0100

    Usuario schema y controlador

---
+++ restapi/usuarios/ModelUsuario.ts
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+const{model,Schema}=mongoose;
+
+const schemaUsuario=new Schema({
+    id:Number,
+    name:String,
+    surname:String,
+    usuario:String,
+    contraseña:String,
+
+})
+schemaUsuario.set('toJSON',{
+    transform:(objeto1:any,objeto2:any)=>{
+        objeto2.id=objeto1._id
+        delete objeto2._id
+        delete objeto2.__v
+    }
+})
+
+
+export const modeloUsuario=mongoose.model('Usuario',schemaUsuario)
