98b12d518e4cc6b1807a61267527b58635f889d9
uo270235 <uo270235@uniovi.es>
2022-03-13 19:01:39 +0100

    barra de menu añadida a pagina inicio

--- webapp/src/pages/mainPage/Homepage.tsx
+++ webapp/src/pages/mainPage/Homepage.tsx
@@ -1,11 +1,13 @@
 import "./homepage.css";
 import ListaProductos from './ListaProductos';
+import MenuBar from "../menuBar";
 
 function Init(): JSX.Element {
 
 
     return (
         <div className="encabezado">
+            <MenuBar/>
             <h1>AsturShop</h1>
             <ListaProductos/>
         </div>
