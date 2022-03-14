c4cbdcb114d4fbfd9c0261a62a86557040a6a771
Héctor Lavandeira <79642990+UO277303@users.noreply.github.com>
2022-03-14 15:51:17 +0100

    Eliminados imports no usados

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
 import LogginForm from './pages/logginPage/LogginForm';
-import UserList from './components/UserList';
-import Homepage from './pages/mainPage/Homepage';
-import CardProducto from './pages/mainPage/CardProducto';
 import  {getUsers} from './api/api';
-import MenuBar from './pages/menuBar';
 import {User} from './shared/shareddtypes';
 import './App.css';
 import Bienvenida from './pages/bienvenidaPage/bienvenidaPage';
