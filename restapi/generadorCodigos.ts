23845f286ebb58fc1a03048d42fd08c898064651
UO277646 <UO277646@uniovi.es>
2022-04-02 19:43:56 +0200

    Generador de ints en vez de doubles.

--- restapi/generadorCodigos.ts
+++ restapi/generadorCodigos.ts
@@ -1,36 +1,41 @@
 import{modeloUsuario} from "./usuarios/modeloUsuario";
 import {modeloProducto} from "./productos/modeloProducto";
 import {modeloCarrito} from "./carritos/ModelCarrito";
+import {Int32} from "mongodb";
 export async function generarCodigoUsuario():Promise<String>{
-    let numero:Number=Math.random() * (3000 - 0) + 0;
+    let numero:Number=getRandomInt();
     let codigo:String="cl"+numero;
     let usuario=modeloUsuario.findOne({"id":codigo});
     while(!usuario){
-        numero=Math.random() * (3000 - 0) + 0;
+        numero=getRandomInt();
         codigo="cl"+numero;
         usuario=modeloUsuario.findOne({"id":codigo})
     }
     return codigo;
 }
 export async function generarCodigoCarrito():Promise<String>{
-    let numero:Number=Math.random() * (3000 - 0) + 0;
+    let numero:Number=getRandomInt();
     let codigo:String="car"+numero;
     let usuario=modeloCarrito.findOne({"id":codigo});
     while(!usuario){
-        numero=Math.random() * (3000 - 0) + 0;
+        numero=getRandomInt();
         codigo="car"+numero;
         usuario=modeloCarrito.findOne({"id":codigo})
     }
     return codigo;
 }
 export async function generarCodigoProducto():Promise<String>{
-    let numero:Number=Math.random() * (3000 - 0) + 0;
+    let numero:Number=getRandomInt();
     let codigo:String="pr"+numero;
     let usuario=modeloProducto.findOne({"id":codigo});
     while(!usuario){
-        numero=Math.random() * (3000 - 0) + 0;
+        numero=getRandomInt();
         codigo="pr"+numero;
         usuario=modeloProducto.findOne({"id":codigo})
     }
     return codigo;
+}
+
+function getRandomInt() {
+    return Math.floor(Math.random() * (3000 - 0)) + 0;
 }
