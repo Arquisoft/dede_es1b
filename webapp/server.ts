ce7e2689be4f6d82f7172a34c12c637f1f0864c4
UO276843 <uo276843@uniovi.es>
2022-04-04 15:34:24 +0200

    intento de deploy 12

--- webapp/server.ts
+++ webapp/server.ts
@@ -7,7 +7,7 @@ const port: number = 3000;
 
 app.use(express.static('build'))
 
-app.listen(port, ():void => {
+app.listen(port ||process.env.PORT, ():void => {
     console.log('Webapp started on port '+ port);
 }).on("error",(error:Error)=>{
     console.error('Error occured: ' + error.message);
