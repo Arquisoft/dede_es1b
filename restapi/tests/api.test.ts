edf5f9701aa997c4502b7f924be06b1aa4749d02
UO277646 <UO277646@uniovi.es>
2022-04-04 20:41:13 +0200

    Intento de test pero fallo (Issue)

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -3,6 +3,8 @@ import express, { Application } from 'express';
 import * as http from 'http';
 import bp from 'body-parser';
 import cors from 'cors';
+import productRouter from '../productos/routerProductos';
+import userRouter from '../usuarios/routerUsuario';
 import api from '../api';
 
 let app:Application;
@@ -17,6 +19,7 @@ beforeAll(async () => {
     app.use(cors(options));
     app.use(bp.json());
     app.use("/api", api)
+    app.use("/products",productRouter)
 
     server = app.listen(port, ():void => {
         console.log('Restapi server for testing listening on '+ port);
@@ -47,14 +50,31 @@ describe('user ', () => {
         expect(response.statusCode).toBe(200);
         expect(response.type).toBe("application/json");
     });
-
-    /**
-     * Tests that a user can be created through the productService without throwing any errors.
-     */
-    it('can be created correctly', async () => {
-        let username:string = 'Pablo'
-        let email:string = 'gonzalezgpablo@uniovi.es'
-        const response:Response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
+    it('podemos sacar una lista de usuarios',async () => {
+        const response:Response = await request(app).get('/Usuarios/list');
         expect(response.statusCode).toBe(200);
+        expect(response.type).toBe("application/json");
     });
+    /**
+     * Test that we can list products without any error.
+     */
+    it('podemos logearnos',async () => {
+        let usuario1:String="admin";
+        let contrasenia1:String="admin";
+        const response:Response = await request(app).post('/usuarios/login').send({usuario:usuario1,contraseña:contrasenia1});
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe("application/json");
+    });
+    it('podemos añadir un usuario',async () => {
+        let nombre:String="Juan";
+        let apellidos="Dominguez";
+        let usuario1="usuarioPrueba";
+        let contraseña1="nosec";
+        let correo1="micorreo.es";
+        const response:Response = await request(app).post('/usuarios/add').send({nombre:nombre,apellidos:apellidos,usuario:usuario1,contraseña:contraseña1,correo:correo1});
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe("application/json");
+    });
+
+
 });
