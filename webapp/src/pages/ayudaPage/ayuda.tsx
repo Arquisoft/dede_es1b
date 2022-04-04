1cb849e69059fcc7f6a7a7bd6ce12f2c24d88bd2
uo270235 <uo270235@uniovi.es>
2022-04-04 22:26:46 +0200

    mejorada ventana ayuda

--- webapp/src/pages/ayudaPage/ayuda.tsx
+++ webapp/src/pages/ayudaPage/ayuda.tsx
@@ -5,6 +5,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MenuBar from "../menuBar";
+import Divider from '@mui/material/Divider';
 
 export default function SimpleAccordion() {
   return (
@@ -14,14 +15,15 @@ export default function SimpleAccordion() {
         
         <h1>Preguntas Frecuentes</h1>
           <div className='acordeones'>
-        <Accordion sx={{width:'600px'}}>
+        <Accordion >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>¿Cómo puedo devolver un producto?</Typography>
+          <Typography><strong>¿Cómo puedo devolver un producto?</strong></Typography>
         </AccordionSummary>
+        <Divider></Divider>
         <AccordionDetails>
           <Typography>
           Lamentablemente aun no aceptamos devoluciones en nuestra tienda online, asi que piense bien antes de comprar algun producto.
@@ -29,59 +31,64 @@ export default function SimpleAccordion() {
         </AccordionDetails>
       </Accordion >
       <br></br>
-      <Accordion sx={{width:'600px'}}>
+      <Accordion >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>¿De qué maneras puedo contactar con AsturShop?</Typography>
+          <Typography><strong>¿De qué maneras puedo contactar con AsturShop?</strong></Typography>
         </AccordionSummary>
+        <Divider></Divider>
         <AccordionDetails>
           <Typography>
-          Mande un correo a asturshop@gmail.com y nosotros le responderemos con una sonrisa.
+          Mande un correo a asturshopDede@gmail.com y nosotros le responderemos con una sonrisa.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <br></br>
-      <Accordion sx={{width:'600px'}}>
+      <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>¿Cuánto cuestan los gastos de envío?</Typography>
+          <Typography><strong>¿Cuánto cuestan los gastos de envío?</strong></Typography>
         </AccordionSummary>
+        <Divider></Divider>
         <AccordionDetails>
           <Typography>
             Depende de varios factores, se le informara un vez que rellene los datos de compra.
           </Typography>
         </AccordionDetails>
       </Accordion>
+
       <br></br>
-      <Accordion sx={{width:'600px'}}>
+      <Accordion >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>¿Cómo puedo presentar una reclamación?</Typography>
+          <Typography><strong>¿Cómo puedo presentar una reclamación?</strong></Typography>
         </AccordionSummary>
+        <Divider></Divider>
         <AccordionDetails>
           <Typography>
-          Podrás reclamar a través del siguiente correo electrónico: asturshop@gmail.com. 
+          Podrás reclamar a través del siguiente correo electrónico: asturshopDede@gmail.com. 
           Las partes se someten, a su elección, para la resolución de los conflictos y con renuncia a cualquier otro fuero, a los juzgados y tribunales del domicilio del usuario.
           </Typography>
         </AccordionDetails>
       </Accordion><br></br>
-      <Accordion sx={{width:'600px'}}>
+      <Accordion >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>¿Puedo envolver productos para regalo?</Typography>
+          <Typography><strong>¿Puedo envolver productos para regalo?</strong></Typography>
         </AccordionSummary>
+        <Divider></Divider>
         <AccordionDetails>
           <Typography>
             De momento no, trabajaremos en ello para poder hacerlo en el futuro.
@@ -89,6 +96,11 @@ export default function SimpleAccordion() {
         </AccordionDetails>
       </Accordion>
       </div>
+      <br></br>
+          <br></br>
+          <br></br>
     </div>
+         
+
   );
 }
