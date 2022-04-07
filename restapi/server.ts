8ec9ba5e816e528566562b2de6ea9e0511e8dfb0
UO276843 <uo276843@uniovi.es>
2022-04-07 14:15:30 +0200

    arreglo permisos

--- restapi/server.ts
+++ restapi/server.ts
@@ -20,11 +20,12 @@ var port= process.env.PORT || 5000;
 
 
 const options: cors.CorsOptions = {
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3000','https://dede-es1b.herokuapp.com']
 };
 
 const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
+
 
 app.use(cors(options));
 app.use(bp.json());
