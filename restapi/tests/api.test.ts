ad27a21b41a9d478ce1c06992b27889fb9da9b49
UO276843 <uo276843@uniovi.es>
2022-04-11 22:35:35 +0200

    Arreglo test restapi

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -4,10 +4,11 @@ import * as http from 'http';
 import bp from 'body-parser';
 import cors from 'cors';
 import api from '../api';
-
+import apiUsuario from '../usuarios/routerUsuario';
 let app:Application;
 let server:http.Server;
-
+const mongoose = require('mongoose');
+const connectString = 'mongodb+srv://uo277646:ADMSIS123$@databasepasturianosy.pzy6r.mongodb.net/dataBase1?retryWrites=true&w=majority';
 beforeAll(async () => {
     app = express();
     const port: number = 5000;
@@ -17,34 +18,33 @@ beforeAll(async () => {
     app.use(cors(options));
     app.use(bp.json());
     app.use("/api", api)
+    app.use(apiUsuario);
+    
 
     server = app.listen(port, ():void => {
         console.log('Restapi server for testing listening on '+ port);
     }).on("error",(error:Error)=>{
         console.error('Error occured: ' + error.message);
     });
+    mongoose.connect(connectString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
 });
 
 afterAll(async () => {
     server.close() //close the server
+    mongoose.connection.close();
 })
 
-describe('user ', () => {
+describe('usuarios', () => {
     /**
      * Test that we can list users without any error.
      */
     it('can be listed',async () => {
-        const response:Response = await request(app).get("/api/users/list");
+        const response:Response = await request(app).get("/usuarios/list");
         expect(response.statusCode).toBe(200);
     });
 
-    /**
-     * Tests that a user can be created through the productService without throwing any errors.
-     */
-    it('can be created correctly', async () => {
-        let username:string = 'Pablo'
-        let email:string = 'gonzalezgpablo@uniovi.es'
-        const response:Response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
-        expect(response.statusCode).toBe(200);
-    });
+    
 });
