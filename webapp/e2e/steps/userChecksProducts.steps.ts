edff89b9c03c44a9fb8496484cb442182d2cd251
Lucia Garcia Sopeña <uo271080@uniovi.es>
2022-05-01 23:58:05 +0200

    test end2end ver productos

---
+++ webapp/e2e/steps/userChecksProducts.steps.ts
@@ -0,0 +1,49 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import puppeteer from "puppeteer";
+
+const feature = loadFeature('./features/userChecksProducts.feature');
+
+let page: puppeteer.Page;
+let browser: puppeteer.Browser;
+
+defineFeature(feature, test => {
+  
+  beforeAll(async () => {
+    browser = process.env.GITHUB_ACTIONS
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+
+    await page
+      .goto("http://localhost:3000", {
+        waitUntil: "networkidle0",
+      })
+      .catch(() => {});
+  });
+
+  test('User accesses to the webpage', ({given,when,then}) => {
+
+    given('a user on welcome page', async () => {
+      const welcomePageText = await page.evaluate(()=> document.body.textContent);
+      expect(welcomePageText).toMatch("Bienvenido a AsturShop");
+    });
+
+    when('user accesses main page', async () => {
+      await expect(page).toClick('a',{text:"Empezar"})
+      await expect(page).toFillForm('form[name="register"]', {
+        
+      })
+      await expect(page).toClick('button', { text: 'Accept' })
+    });
+
+    then('A confirmation message should be shown in the screen', async () => {
+      await expect(page).toMatch('You have been registered in the system!')
+    });
+  })
+
+  afterAll(async ()=>{
+    browser.close()
+  })
+
+});
+
